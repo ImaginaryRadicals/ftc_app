979f3e4394efba8c968b116ee2d907921735dea7
AJTrowell <ajtrowell@gmail.com>
2016-11-10 23:41:22 -0600

    Add EncoderNavigation class. Updated Hardware_iRads to match.
    EncoderNavigation is a new class for navigating by integrating
    encoder steps overtime, and calculating delta X,Y,Heading.
    This class implements the basic functionality, but requires
    additional features to work in concert with the rest of the project.
    Methods:
        initialize(Hardware_iRads robot)
        setSteps(int stepsLeft, instepsRight)
        printResults()
    All 3 methods require updates to be compatable.
    
    
    Hardware_iRads has been updated with new constants needed for
    EncoderNavigation, including WHEELBASE_WIDTH_MM and
    DRIVE_WHEEL_MM_PER_ROT. Some (unused) naming conventions were adjusted.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EncoderNavigation.java
@@ -0,0 +1,189 @@
+package org.firstinspires.ftc.teamcode;
+
+/**
+ * Created by Ashley on 11/10/2016.
+ */
+
+public class EncoderNavigation {
+
+
+    public Hardware_iRads robot = null; // Robot Hardware class
+
+    public enum RotationMode {SAME_DIRECTION, OPPOSITE_DIRECTION};
+    public enum RotationCenter {LEFT_SIDE, RIGHT_SIDE, CENTERED};
+    public enum RobotDirection {FORWARD, REVERSE}; //
+//    public enum BodyRotation {CCW, CW, NO);
+
+    // Aggregated Inputs:
+    private int stepsLeft = 0;
+    private int stepsRight = 0;
+
+    // Aggregated outputs: (should all be private with get() methods.)
+    private double outputTimestamp = -1; // seconds
+    private RotationMode rotationMode; // Wheels spining in same or opposite directions?
+    private RotationCenter rotationCenter; // which side is the rotation point on?
+    //    public BodyRotation bodyRotation; // CCW, CW, or NONE
+    private double radiusFromCenter_mm = 0; // Positive to robot's right side.
+    private double arcDistance_mm = 0;
+    private double deltaHeading_deg = 0; //Positive CCW
+    // RobotX to right, RobotY in driving direction. (for this class, anyhow.)
+    private double deltaRobotX_mm = 0;
+    private double deltaRobotY_mm = 0;
+
+
+
+    // Constructor
+    public EncoderNavigation()
+    {
+
+    }
+
+    public void initialize(Hardware_iRads robot) {
+        this.robot = robot;
+    }
+
+
+    public void setSteps(int stepsLeft, int stepsRight) {
+        this.stepsLeft = stepsLeft;
+        this.stepsRight = stepsRight;
+        //this.outputTimestamp = runtime.time();
+        this.outputTimestamp = System.nanoTime()/Math.pow(10,9);
+        calculateResults();
+    }
+
+    // Return age of encoder position in seconds.
+    public double getAge() {
+        return (System.nanoTime()/Math.pow(10,9) - this.outputTimestamp);
+    }
+
+    public void printResults() {
+        System.out.println( "" ); // newline
+        System.out.println( "Status" );
+        System.out.println( String.format("INPUTS: stepsLeft: %d, stepsRight: %d",stepsLeft,stepsRight) );
+        System.out.println( String.format("%.0f mm from center.",radiusFromCenter_mm) );
+        System.out.println(rotationMode);
+        System.out.println(rotationCenter); // sign of radiusFromCenter has same info. Redundant.
+
+        System.out.println( String.format("%.0f mm arcLength deltaHeading",arcDistance_mm) );
+        System.out.println( String.format("%.1f deg deltaHeading",deltaHeading_deg) ); // positive CCW
+        System.out.println( String.format("%.1f mm RobotX.",deltaRobotX_mm) );
+        System.out.println( String.format("%.1f mm RobotY.",deltaRobotY_mm) );
+
+        // transform to game from Robot coordinates to Game Areana coordinates for: X,Y,Heading,
+        //given initial robot position in game X,Y,Heading.
+
+    }
+
+    public void calculateResults()
+    {
+        double r1_mm = 0; //radius small
+        double r2_mm = 0; //radius large
+        int s1 = 0; //steps small
+        int s2 = 0; //steps large
+        double r_center_mm = 0; // positive radius to right of robot.
+
+
+        // Check whether wheels are spinning the same direction or opposite directions.
+        if (stepsLeft * stepsRight >= 0) {
+            rotationMode = RotationMode.SAME_DIRECTION;
+        }
+        else {
+            rotationMode = RotationMode.OPPOSITE_DIRECTION;
+        }
+
+        // Find larger/smaller step magnitude.
+        if ( Math.abs(stepsRight) > Math.abs(stepsLeft) ) {
+            rotationCenter = RotationCenter.LEFT_SIDE;
+            s1 = Math.abs(stepsLeft);
+            s2 = Math.abs(stepsRight);
+        }
+        else if ( Math.abs(stepsRight) < Math.abs(stepsLeft) ) {
+            rotationCenter = RotationCenter.RIGHT_SIDE;
+            s1 = Math.abs(stepsRight);
+            s2 = Math.abs(stepsLeft);
+        }
+        else {
+            rotationCenter = RotationCenter.CENTERED;
+            s1 = Math.abs(stepsLeft);
+            s2 = Math.abs(stepsRight);
+        }
+
+        // During motion through arc, will the body rotate CCW, CW, or Not at all?
+        // DEBUG: Is this used at all?
+
+        // OR, determine whether arc motion is 'forward' or 'backwards?'
+        //if ( Math.max( )
+
+        if (rotationMode == RotationMode.SAME_DIRECTION) {
+            r1_mm = robot.WHEELBASE_WIDTH_MM * s1/(s2-s1);
+            r2_mm = robot.WHEELBASE_WIDTH_MM + r1_mm;
+            r_center_mm = (r1_mm + r2_mm)/2;
+        }
+        else if (rotationMode == RotationMode.OPPOSITE_DIRECTION) {
+            r1_mm = robot.WHEELBASE_WIDTH_MM * s1/(s2+s1);
+            r2_mm = robot.WHEELBASE_WIDTH_MM - r1_mm;
+            r_center_mm = (r2_mm-r1_mm)/2;
+        }
+        else {
+            System.out.println("Wrong rotationMode value.");
+        }
+
+        // output to radiusFromCenter_mm object member
+        switch (rotationCenter) {
+            case LEFT_SIDE:
+                radiusFromCenter_mm = -r_center_mm;
+                break;
+            case CENTERED:
+                radiusFromCenter_mm = r_center_mm;
+                break;
+            case RIGHT_SIDE:
+                radiusFromCenter_mm = r_center_mm;
+                break;
+            default:
+                System.out.println("DEFAULT return switch. Shouldn't happen.");
+                radiusFromCenter_mm = r_center_mm;
+                break;
+        } // switch
+
+        // calculate arcDistance.  Positive for robot moving 'forward'.
+        // arc length of robot centerline motion.
+        // calculate delta heading, X, and Y in initial robot frame.
+        // RobotX to right, RobotY in driving direction. (for this class, anyhow.)
+        arcDistance_mm = (stepsLeft + stepsRight)/2.0*(robot.DRIVE_WHEEL_MM_PER_ROT/robot.DRIVE_WHEEL_STEPS_PER_ROT);
+        switch (rotationCenter) {
+            case LEFT_SIDE:
+                // same for both sides
+                deltaHeading_deg = -(arcDistance_mm / radiusFromCenter_mm) * (180 /  Math.PI);
+                deltaRobotY_mm =  -radiusFromCenter_mm * Math.sin(deltaHeading_deg * (Math.PI/180));
+                // unique
+                deltaRobotX_mm = radiusFromCenter_mm * (1 - Math.cos(deltaHeading_deg * (Math.PI/180)));
+                break;
+            case RIGHT_SIDE:
+                // same for both sides
+                deltaHeading_deg = -(arcDistance_mm / radiusFromCenter_mm) * (180 /  Math.PI);
+                deltaRobotY_mm =  -radiusFromCenter_mm * Math.sin(deltaHeading_deg * (Math.PI/180));
+                // unique
+                deltaRobotX_mm = radiusFromCenter_mm * (1 - Math.cos(deltaHeading_deg * (Math.PI/180)));
+                break;
+            case CENTERED:
+                switch (rotationMode) {
+                    case SAME_DIRECTION:
+                        deltaHeading_deg = 0;
+                        deltaRobotX_mm = 0;
+                        deltaRobotY_mm = arcDistance_mm;
+                        break;
+                    case OPPOSITE_DIRECTION:
+                        deltaHeading_deg = (( stepsRight * (robot.DRIVE_WHEEL_MM_PER_ROT / robot.DRIVE_WHEEL_STEPS_PER_ROT )/r1_mm) * (180/Math.PI));
+                        deltaRobotX_mm = 0;
+                        deltaRobotY_mm = 0;
+                        break;
+                } // switch rotationMode
+                break;
+        } // switch rotationCenter
+
+
+    } // getRadius()
+
+
+
+} // EncoderNavigation
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware_iRads.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware_iRads.java
@@ -40,9 +40,15 @@ public class Hardware_iRads
     public static final double LAUNCH_WHEEL_DIAMETER_INCHES =  4;
     public static final double DRIVE_WHEEL_DIAMETER_INCHES  =  4;
 
-    public static final int TICKS_PER_ROT_DRIVE     =  28*60; // Ticks per rotation
-    public static final int TICKS_PER_ROT_LIFT      =  28*60; // Ticks per rotation
-    public static final int TICKS_PER_ROT_LAUNCH    =  28*1 ; // Ticks per rotation
+    public static final double MM_PER_IN = 25.4f;
+    public static final double WHEELBASE_WIDTH_IN = 18;
+    public static final double WHEELBASE_WIDTH_MM  = WHEELBASE_WIDTH_IN  * MM_PER_IN;
+    public static final double DRIVE_WHEEL_RADIUS_MM = DRIVE_WHEEL_DIAMETER_INCHES /2.0 * MM_PER_IN;
+    public static final double DRIVE_WHEEL_MM_PER_ROT = DRIVE_WHEEL_RADIUS_MM * 2 *  Math.PI;
+
+    public static final int DRIVE_WHEEL_STEPS_PER_ROT     = 28*60;
+    public static final int LAUNCH_WHEEL_STEPS_PER_ROT    =  28*1 ; // Ticks per rotation
+    public static final int LIFT_STEPS_PER_ROT            =  28*60; // Ticks per rotation
 
     public static final int MAX_DRIVE_SPEED_TPS     =  1680 ; // Ticks per second
     public static final int LIFT_MAX_SPEED_TPS      =  1680 ; // Ticks per second
