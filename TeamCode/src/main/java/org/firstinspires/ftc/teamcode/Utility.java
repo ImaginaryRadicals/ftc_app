981dbd644d1bb0400230323a8160315167594c94
elfergo <iamwillferguson@gmail.com>
2016-11-25 14:27:13 -0600

    Prevented Divide by 0 in Expo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Utility.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Utility.java
@@ -10,7 +10,7 @@ public class Utility {
     static double expo(double input, double gain) {
         // y = (x^3 * (k-1) + x ) / k
         // formula for exponential drive
-
+        gain = Range.clip(gain, 1,1000);
         double output = ((Math.pow(input, 3) * (gain-1) + input)/gain);
         return Range.clip(output, -1,1);
 
