8887319712d93dc08a8f824fce50753ccce00134
fantasticMan1 <joshua.winebrener@gmail.com>
2016-11-25 14:08:32 -0600

    Added Signal class to detect rising edge of a digital signal

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Signal.java
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode;
+
+/**
+ * Created by Josh Winebrener on 11/25/2016.
+ */
+
+public class Signal {
+    public boolean lastState     = false;
+    public boolean currentState  = false;
+
+    public void updateState(boolean newState) {
+        lastState    = currentState;
+        currentState = newState;
+    }
+
+    public boolean risingEdge(){
+        if (lastState == false && currentState == true) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
