a98e129601ed8f7bf57d028184f86f4e13f9814f
elfergo <iamwillferguson@gmail.com>
2016-11-25 14:21:29 -0600

    Added Utility.expo()
    Useful for exponential gain on joysticks

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Utility.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.util.Range;
+
+/**
+ * Created by aferg on 11/25/2016.
+ */
+
+public class Utility {
+    static double expo(double input, double gain) {
+        // y = (x^3 * (k-1) + x ) / k
+        // formula for exponential drive
+
+        double output = ((Math.pow(input, 3) * (gain-1) + input)/gain);
+        return Range.clip(output, -1,1);
+
+    }
+}
