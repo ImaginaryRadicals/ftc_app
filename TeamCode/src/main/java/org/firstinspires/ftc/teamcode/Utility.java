485b351e5e1cf8331dd084f99cea64c93dd3b541
fantasticMan1 <joshua.winebrener@gmail.com>
2016-12-17 10:09:24 -0600

    Added getMotorTicks method
    Used a vector to record encoder values relative to time and averaged the past 30 values to find the rate of the motor.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Utility.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Utility.java
@@ -1,12 +1,20 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.util.Range;
+import java.util.Vector;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 /**
  * Created by aferg on 11/25/2016.
  */
 
 public class Utility {
+
+    static Vector pastMotorRates = new Vector();
+
+    public static double lastEncoderPosition = 0;
+    public static double lastElapsedTime = 0;
+
     static double expo(double input, double gain) {
         // y = (x^3 * (k-1) + x ) / k
         // formula for exponential drive
@@ -14,5 +22,29 @@ public class Utility {
         double output = ((Math.pow(input, 3) * (gain-1) + input)/gain);
         return Range.clip(output, -1,1);
 
+    }
+
+    public static double getMotorTickRate(DcMotor motorInQuestion, double elapsedTime) {
+
+        double currentEncoderPosition = motorInQuestion.getCurrentPosition();
+        double motorTickRate = 0;
+        double currentElapsedTime = 0;
+
+        double deltaEncoderPositions = currentEncoderPosition - lastEncoderPosition;
+        double deltaElapsedTime = currentElapsedTime - lastElapsedTime;
+        double rate = deltaEncoderPositions / deltaElapsedTime;
+
+        pastMotorRates.add(rate);
+
+        if (pastMotorRates.size() >= 30) {
+            pastMotorRates.remove(0);
+        }
+
+        motorTickRate = VectorMath.average(pastMotorRates);
+
+        lastEncoderPosition = currentEncoderPosition;
+        lastElapsedTime = currentElapsedTime;
+
+        return motorTickRate;
     }
 }
