e75c84422162c2fdb26dc7bec694bac61889f7e2
Ryder Swan <oars@bartswan.com>
2017-01-21 09:43:34 -0500

    Changed the sleep to 2000 mm to allow kinetic energy create.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/iRadsSimpleAutoOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/iRadsSimpleAutoOp.java
@@ -155,7 +155,7 @@ public class iRadsSimpleAutoOp extends LinearOpMode {
         // Close the flippers to prevent the second particle from escaping
         robot.leftFlipper.setPosition(robot.LEFT_FLIPPER_CLOSED);
         robot.rightFlipper.setPosition(robot.RIGHT_FLIPPER_CLOSED);
-        sleep(1000);
+        sleep(2000);
 
         //launch a second time
         robot.launchTrigger.setPosition(robot.ELEVATED_LAUNCHER_TRIGGER_POS);
