80b66f855028277dd16209ee2533d39e7adc248a
Ryder Swan <oars@bartswan.com>
2017-02-11 14:36:35 -0600

    Working Driving
    One stick Drive, - One stick Drive, and Tank Drive

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/iRadsAutoOpMode_Linear.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/iRadsAutoOpMode_Linear.java
@@ -221,14 +221,8 @@ public class iRadsAutoOpMode_Linear extends LinearOpMode {
         } else
             ManualControl.setSingleStickXY(-gamepad1.left_stick_x, -gamepad1.left_stick_y);
 
+        
 
-
-/*        if (gamepad1.left_trigger > 0.5) { // Tank drive if left trigger pulled
-            // Left and Right sticks control tank drive.
-            nextMotorState.leftDriveMotor = Utility.expo(-gamepad1.left_stick_y, expoGain);
-            nextMotorState.rightDriveMotor = Utility.expo(-gamepad1.right_stick_y, expoGain);
-
-*/
         {
             // Left (single)stick control, defalt.
             nextMotorState.leftDriveMotor = ManualControl.leftDriveMotorPower;
@@ -238,6 +232,11 @@ public class iRadsAutoOpMode_Linear extends LinearOpMode {
         }
 
 
+        if (gamepad1.left_trigger > 0.5) { // Tank drive if left trigger pulled
+            // Left and Right sticks control tank drive.
+            nextMotorState.leftDriveMotor = Utility.expo(-gamepad1.left_stick_y, expoGain);
+            nextMotorState.rightDriveMotor = Utility.expo(-gamepad1.right_stick_y, expoGain);
+        }
 
         // Use gamepad buttons to move the fork lift up (Y) and down (A)
         if (gamepad1.y) {
