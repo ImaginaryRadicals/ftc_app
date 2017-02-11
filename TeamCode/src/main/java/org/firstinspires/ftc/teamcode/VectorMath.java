a3510e02d08973634b4974fd1aa43e85f69a77fc
Stephen Berry <stephenberry.developer@gmail.com>
2017-02-11 12:40:14 -0600

    Added derivative functions for equal or unequally spaced data

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VectorMath.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VectorMath.java
@@ -60,4 +60,36 @@ public class VectorMath
             return 0.0;
         }
     }
+
+    // Derivative
+    // x is the independant variable, y is dependent. Returns derivative for unequally spaced points. xest is the x value at which to evaluate the derivative.
+    // x and y vectors don't need to be the same length if they have three elements or more each
+    static public double derivative(Vector<Double> x, Vector<Double> y, double xest) {
+
+        int nx = x.size();
+        int ny = y.size();
+        if (x.size() < 2 || y.size() < 2) {
+            return 0.0;
+        } else if (nx == 2 || ny == 2) {
+            return (y.elementAt(1) - y.elementAt(0)) / (x.elementAt(1) - x.elementAt(0));
+        } else {
+            double x2 = x.elementAt(nx - 1);
+            double x1 = x.elementAt(nx - 2);
+            double x0 = x.elementAt(nx - 3);
+
+            double y2 = y.elementAt(ny - 1);
+            double y1 = y.elementAt(ny - 2);
+            double y0 = y.elementAt(ny - 3);
+
+            double dydx = y0 * (2.0 * xest - x1 - x2) / ((x0 - x1) * (x0 - x2)) + y1 * (2.0 * xest - x0 - x2) / ((x1 - x0) * (x1 - x2)) + y2 * (2.0 * xest - x0 - x1) / ((x2 - x0) * (x2 - x1));
+
+            return dydx;
+        }
+    }
+
+    // Default derivative computation at the latest x value
+    static public double derivative(Vector<Double> x, Vector<Double> y) {
+
+        return derivative(x, y, x.lastElement());
+    }
 }
