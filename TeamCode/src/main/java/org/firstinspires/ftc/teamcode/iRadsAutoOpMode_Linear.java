a1658af184aa0385db72149b41d6d9b920294646
Ryder Swan <oars@bartswan.com>
2017-02-11 16:50:09 -0600

    Commited out tank drive mode.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/iRadsAutoOpMode_Linear.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/iRadsAutoOpMode_Linear.java
@@ -237,12 +237,12 @@ public class iRadsAutoOpMode_Linear extends LinearOpMode {
             telemetry.addData("AngleDeg", ManualControl.angleDeg);
         }
 
-        if (gamepad1.left_trigger > 0.5) { // Tank drive if left trigger pulled
+/*        if (gamepad1.left_trigger > 0.5) { // Tank drive if left trigger pulled
             // Left and Right sticks control tank drive.
             nextMotorState.leftDriveMotor = Utility.expo(-gamepad1.left_stick_y, expoGain);
             nextMotorState.rightDriveMotor = Utility.expo(-gamepad1.right_stick_y, expoGain);
         }
-
+*/
         
         // Use gamepad buttons to move the fork lift up (Y) and down (A)
         if (gamepad1.y) {
