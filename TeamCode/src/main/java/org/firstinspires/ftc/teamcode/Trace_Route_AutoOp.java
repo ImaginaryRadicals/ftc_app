4611934bb3a9baffab6553ca87d5ce05987fc33f
JoshWinebrener <joshua.winebrener@gmail.com>
2017-02-11 20:49:05 -0600

    Added and named Trace_Route_AutoOp

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Trace_Route_AutoOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Trace_Route_AutoOp.java
@@ -32,29 +32,18 @@ THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 /**
- * This file contains an minimal example of a Linear "OpMode". An OpMode is a 'program' that runs in either
- * the autonomous or the teleop period of an FTC match. The names of OpModes appear on the menu
- * of the FTC Driver Station. When an selection is made from the menu, the corresponding OpMode
- * class is instantiated on the Robot Controller and executed.
  *
- * This particular OpMode just executes a basic Tank Drive Teleop for a PushBot
- * It includes all the skeletal structure that all linear OpModes contain.
- *
- * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
- * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
 
-@TeleOp(name="Trace route Teleop", group="Linear Opmode")  // @Autonomous(...) is the other common choice
+@Autonomous(name="Trace Route Autoop", group="Linear Opmode")
 //@Disabled
-public class Trace_Route_TeleOp extends LinearOpMode {
+public class Trace_Route_AutoOp extends LinearOpMode {
 
     /* Declare OpMode members. */
     private ElapsedTime runtime = new ElapsedTime();
