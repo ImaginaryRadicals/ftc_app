30730d1ceb24e5744cc6763716cf5d1fe7313cd5
Stephen Berry <stephenberry.developer@gmail.com>
2017-02-18 13:42:58 -0600

    Added comments to PID

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PID.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PID.java
@@ -9,13 +9,14 @@ import java.util.Vector;
 
 public class PID
 {
-    private Vector<Double> time = new Vector<>();
-    private Vector<Double> error = new Vector<>();
-    public Double Kp = new Double(0.0);
-    public Double Ki = new Double(0.0);
-    public Double Kd = new Double(0.0);
+    private Vector<Double> time = new Vector<>(); // time history
+    private Vector<Double> error = new Vector<>(); // error history
+    public Double Kp = new Double(0.0); // Proportional gain
+    public Double Ki = new Double(0.0); // Integral gain
+    public Double Kd = new Double(0.0); // Derivative gain
     public Double sample_time = new Double(1.0);
 
+    // pass in the current time point with the current error value
     public void addError(Double t, Double e)
     {
         time.add(t);
@@ -28,6 +29,7 @@ public class PID
         }
     }
 
+    // retrieve the controller gain (call this after addError)
     public Double gain()
     {
         return Kp * error.lastElement() + Ki * VectorMath.integral(time, error)
